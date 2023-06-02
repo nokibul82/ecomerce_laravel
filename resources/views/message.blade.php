a24b503c5a9c659308b2f00c3a60c71dac49abad
nokibul82 <nokibul82@gmail.com>
2023-06-02 22:46:01 +0600

    order added

---
+++ resources/views/message.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+        <span class="font-medium">Success alert!</span> Items ordered succusfully !
+    </div>
+</x-app-layout>
